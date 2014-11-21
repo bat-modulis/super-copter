eefccafaee0ada48cafea5e71d5f29e9dac5573a
Baptiste Cholley <baptiste.cholley@modulis.ca>
2014-11-21 11:59:02 -0500

    design improvment

--- helico.sh
+++ helico.sh
@@ -5,9 +5,9 @@
 c1()
 {
     echo "           _^____      "
-    echo "   L    __/    []\\     "
-    echo "  LOL===_         \\    "
-    echo "   L     \\_________]   "
+    echo "   *    __/    []\\     "
+    echo "  *O*===_         \\    "
+    echo "   *     \\_________]   "
     echo "           I   I       "
     echo "          -------/     "
     sleep 0.1
@@ -15,26 +15,26 @@ c1()
 c2()
 {
     echo "           _^____      "
-    echo "  L L   __/    []\\     "
+    echo "  * *   __/    []\\     "
     echo "   O====_         \\    "
-    echo "  L L    \\_________]   "
+    echo "  * *    \\_________]   "
     echo "           I   I       "
     echo "          -------/     "
     sleep 0.1
 }
 echo -e "\n\n\n\n\n\n\n\n\n\033[8F\033[s"
 while [ 1 = 1 ]
-do  echo -e "\033[u   ROFL:ROFL:ROFL:ROFL " ; c1
-    echo -e "\033[u     LOL:LOL:LOL:LOL   " ; c2
-    echo -e "\033[u        ROFL:ROFL      " ; c1
-    echo -e "\033[u           LOL         " ; c2
-    echo -e "\033[u        ROFL:ROFL      " ; c1
-    echo -e "\033[u     LOL:LOL:LOL:LOL   " ; c2
-    echo -e "\033[u   ROFL:ROFL:ROFL:ROFL " ; c1
-    echo -e "\033[u     LOL:LOL:LOL:LOL   " ; c2
-    echo -e "\033[u        ROFL:ROFL      " ; c1
-    echo -e "\033[u           LOL         " ; c2
-    echo -e "\033[u        ROFL:ROFL      " ; c1
-    echo -e "\033[u     LOL:LOL:LOL:LOL   " ; c2
+do  echo -e "\033[u   ==:==:==:==:==:==:==:== " ; c1
+    echo -e "\033[u     ---:------:------:--- " ; c2
+    echo -e "\033[u        ==:==:==:==:==      " ; c1
+    echo -e "\033[u                    " ; c2
+    echo -e "\033[u        ==:==:==:==:=      " ; c1
+    echo -e "\033[u     ---:------:------:---   " ; c2
+    echo -e "\033[u   ==:==:==:==:==:==:==:== " ; c1
+    echo -e "\033[u     ---:------:------:---   " ; c2
+    echo -e "\033[u        ==:==:==:==      " ; c1
+    echo -e "\033[u                         " ; c2
+    echo -e "\033[u        ==:==:==:==      " ; c1
+    echo -e "\033[u     ---:------:------:---   " ; c2
 done
 
